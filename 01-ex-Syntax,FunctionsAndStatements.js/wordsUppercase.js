0966e7c816506714eb0739b94daf5b05a09d809a
Anita Bucheva <anitabucheva@abv.bg>
2024-02-22 14:04:00 +0200

    Delete wordsUppercase.js

--- 01-ex-Syntax,FunctionsAndStatements.js/wordsUppercase.js
+++
@@ -1,9 +0,0 @@
-function wordsUppercase(string) {
-
-    let pattern = /\w+/g;
-    let newString = string.match(pattern);
-    
-    console.log(newString.join(", ").toUpperCase());
-}
-wordsUppercase('Hi, how are you?') // HI, HOW, ARE, YOU
-wordsUppercase('hello') // HELLO
