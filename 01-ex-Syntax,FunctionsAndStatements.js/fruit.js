75b3ac3ef2a0f838692e5276f4b57141cdf057d2
Anita Bucheva <anitabucheva@abv.bg>
2024-02-22 14:03:38 +0200

    Delete fruit.js

--- 01-ex-Syntax,FunctionsAndStatements.js/fruit.js
+++
@@ -1,8 +0,0 @@
-function fruit(fruit, gram, price) {
-    
-    let weight = gram / 1000;
-    let money = weight * price;
-
-    console.log(`I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`);
-}
-fruit('orange', 2500, 1.80);
