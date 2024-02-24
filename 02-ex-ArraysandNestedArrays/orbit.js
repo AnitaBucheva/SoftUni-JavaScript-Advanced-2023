7b3623a6e50f10830a5f91a8e9fba84e922ffef9
Anita Bucheva <anitabucheva@abv.bg>
2024-02-24 13:52:12 +0200

    Create orbit.js

---
+++ 02-ex-ArraysandNestedArrays/orbit.js
@@ -0,0 +1,9 @@
+function orbit(arr) {
+    let matrix = new Array(arr[0]).fill(new Array(arr[1]).fill(''))
+
+    return matrix
+        .map((x, i) => x.map((y, j) => Math.max(Math.abs(j - arr[3]), Math.abs(i - arr[2])) + 1))
+        .map(x => x.join(" "))
+        .join("\n")
+}
+orbit([4, 4, 0, 0])
