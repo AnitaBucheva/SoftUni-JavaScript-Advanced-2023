801a62c7ce6e44396b45ea1f4c76ae937348b3af
Anita Bucheva <anitabucheva@abv.bg>
2024-03-03 13:40:38 +0200

    Delete collectListItems.js

--- 04-Lab-DOMIntroduction.js/02.Collect List Items/collectListItems.js
+++
@@ -1,7 +0,0 @@
-function extractText() {
-    let element = document.getElementById("items");
-    
-    let result = document.getElementById("result");
-    
-    result.textContent = element.textContent;
-    }
