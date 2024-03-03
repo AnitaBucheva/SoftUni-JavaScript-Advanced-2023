7c6ef6a0538dcc05685b2e18bbf64679a3e25d84
Anita Bucheva <anitabucheva@abv.bg>
2024-03-03 13:40:44 +0200

    Delete sumNumbers.js

--- 04-Lab-DOMIntroduction.js/03.Sum Numbers/sumNumbers.js
+++
@@ -1,9 +0,0 @@
-function calc() {
-    let firstNumber = document.getElementById("num1");
-    let secondNumber = document.getElementById("num2");
-    let result = Number(firstNumber.value) + Number(secondNumber.value);
-  
-    let sum = document.getElementById("sum");
-  
-    sum.value = result;
-  }
